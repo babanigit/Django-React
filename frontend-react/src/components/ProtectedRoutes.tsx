9cfddfb5aaa308d5510e7528eb47c4e8f6604923
aniket panchal <aniketpanchal07@gmail.com>
2024-10-05 23:14:26 +0530

    added frontend

--- frontend-react/src/components/ProtectedRoutes.tsx
+++ frontend-react/src/components/ProtectedRoutes.tsx
@@ -3,13 +3,49 @@ import { jwtDecode } from "jwt-decode";
 
 import api from "../files/api";
 import { REFRESH_TOKEN, ACCESS_TOKEN } from "../files/constants";
-import React, { Children, useState } from "react";
+import React, { Children, useEffect, useState } from "react";
 
-const ProtectedRoutes = ( { children }: { children: React.ReactNode } ) => {
-  const [isAuthorized, setIsAuthorized] = useState(null);
+const ProtectedRoutes = ({ children }: { children: React.ReactNode }) => {
+  const [isAuthorized, setIsAuthorized] = useState(Boolean);
 
-  const refreshToken = async () => {};
-  const auth = async () => {};
+  useEffect(() => {
+    auth().catch(() => setIsAuthorized(false));
+  }, []);
+
+  const refreshToken = async () => {
+    const refreshToken = localStorage.getItem(REFRESH_TOKEN);
+    try {
+      const res = await api.post("/api/token/refresh/", {
+        refresh: refreshToken,
+      });
+      if (res.status === 200) {
+        localStorage.setItem(ACCESS_TOKEN, res.data.access);
+        setIsAuthorized(true);
+      } else {
+        setIsAuthorized(false);
+      }
+    } catch (error) {
+      console.log(error);
+      setIsAuthorized(false);
+    }
+  };
+
+  const auth = async () => {
+    const token = localStorage.getItem(ACCESS_TOKEN);
+    if (!token) {
+      setIsAuthorized(false);
+      return;
+    }
+    const decoded = jwtDecode(token);
+    const tokenExpiration = decoded.exp;
+    const now = Date.now() / 1000;
+
+    if (tokenExpiration! < now) {
+      await refreshToken();
+    } else {
+      setIsAuthorized(true);
+    }
+  };
 
   if (isAuthorized == null) {
     return <div> Loading... </div>;
