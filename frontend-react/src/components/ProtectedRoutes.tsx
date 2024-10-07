2e603791d43448714e7cb1b5ca07f90eebabfc4f
aniket panchal <aniketpanchal07@gmail.com>
2024-10-07 12:32:07 +0530

    done with the react-django

--- frontend-react/src/components/ProtectedRoutes.tsx
+++ frontend-react/src/components/ProtectedRoutes.tsx
@@ -3,10 +3,17 @@ import { jwtDecode } from "jwt-decode";
 
 import api from "../files/api";
 import { REFRESH_TOKEN, ACCESS_TOKEN } from "../files/constants";
-import React, { Children, useEffect, useState } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
+interface ProtectedRouteProps {
+  children: ReactNode;
+}
 
-const ProtectedRoutes = ({ children }: { children: React.ReactNode }) => {
-  const [isAuthorized, setIsAuthorized] = useState(Boolean);
+interface DecodedToken {
+  exp: number;
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+  const [isAuthorized, setIsAuthorized] = useState<boolean | null>(null);
 
   useEffect(() => {
     auth().catch(() => setIsAuthorized(false));
@@ -25,7 +32,7 @@ const ProtectedRoutes = ({ children }: { children: React.ReactNode }) => {
         setIsAuthorized(false);
       }
     } catch (error) {
-      console.log(error);
+      console.error(error);
       setIsAuthorized(false);
     }
   };
@@ -36,22 +43,23 @@ const ProtectedRoutes = ({ children }: { children: React.ReactNode }) => {
       setIsAuthorized(false);
       return;
     }
-    const decoded = jwtDecode(token);
+
+    const decoded = jwtDecode<DecodedToken>(token);
     const tokenExpiration = decoded.exp;
     const now = Date.now() / 1000;
 
-    if (tokenExpiration! < now) {
+    if (tokenExpiration < now) {
       await refreshToken();
     } else {
       setIsAuthorized(true);
     }
   };
 
-  if (isAuthorized == null) {
-    return <div> Loading... </div>;
+  if (isAuthorized === null) {
+    return <div>Loading...</div>;
   }
 
-  return isAuthorized ? <> {children} </> : <Navigate to="/login" />;
+  return isAuthorized ? <>{children}</> : <Navigate to="/login" />;
 };
 
-export default ProtectedRoutes;
+export default ProtectedRoute;
